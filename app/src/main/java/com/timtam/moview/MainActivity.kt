316c780f1c4d591f142c7ad3f56f3429cd6fe43c
DESKTOP-ED6NHR8\PC <timurharyo00@gmail.com>
2022-11-06 22:09:29 +0700

    update implementing example to ui class

--- app/src/main/java/com/timtam/moview/MainActivity.kt
+++ app/src/main/java/com/timtam/moview/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.timtam.moview
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.timtam.common.abstraction.BindingActivity
+import com.timtam.common.delegation.inflater.AsyncInflater
+import com.timtam.common.delegation.inflater.AsyncInflaterImpl
+import com.timtam.moview.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :
+    BindingActivity<ActivityMainBinding>(),
+    AsyncInflater by AsyncInflaterImpl() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        inflate(R.layout.activity_main) { view ->
+            setBinding(ActivityMainBinding.bind(view))
+            setContentView(binding.root)
+        }
     }
 }
