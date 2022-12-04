9a4fcbeecef649da35cdfab9df0830cf00c34cb2
DESKTOP-ED6NHR8\PC <timurharyo00@gmail.com>
2022-12-04 21:00:39 +0700

    add view stub inflater and binding retriever

--- common/src/main/java/com/timtam/common/extension/ViewExt.kt
+++ common/src/main/java/com/timtam/common/extension/ViewExt.kt
@@ -1,6 +1,8 @@
 package com.timtam.common.extension
 
 import android.view.View
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.ViewStubProxy
 
 fun View.visible() {
     this.visibility = View.VISIBLE
@@ -20,4 +22,32 @@ fun View.enable() {
 
 fun View.disable() {
     this.isEnabled = false
+}
+
+fun View.setVisibleElseGone(clause: Boolean) {
+    if (clause) visible() else gone()
+}
+
+inline fun <reified T: ViewDataBinding> bindingStubType(
+    proxy: ViewStubProxy?,
+    crossinline onBind: T.() -> Unit
+) {
+    proxy?.binding?.let { if (it is T) it.onBind() }
+}
+
+inline fun ViewStubProxy.inflateIf(
+    clause: Boolean,
+    crossinline onInflated: () -> Unit
+) {
+    setOnInflateListener { _, _ ->
+        onInflated()
+        binding?.root?.setVisibleElseGone(clause)
+    }
+
+    if (isInflated) {
+        binding?.root?.setVisibleElseGone(clause)
+        return
+    }
+
+    if (clause && !isInflated) viewStub?.inflate()
 }
