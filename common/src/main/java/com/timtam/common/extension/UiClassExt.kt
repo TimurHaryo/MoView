78b7cd51108b80157279114dfe5789bfb6f49e73
DESKTOP-ED6NHR8\PC <timurharyo00@gmail.com>
2022-12-05 07:43:18 +0700

    add greeting function based on time

--- common/src/main/java/com/timtam/common/extension/UiClassExt.kt
+++ common/src/main/java/com/timtam/common/extension/UiClassExt.kt
@@ -1,8 +1,11 @@
 package com.timtam.common.extension
 
 import android.app.Activity
+import android.content.Context
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.timtam.common.R
+import java.util.Calendar
 
 fun Activity.toast(duration: Int = Toast.LENGTH_SHORT, msg: () -> String) {
     Toast.makeText(this, msg(), duration).show()
@@ -14,4 +17,18 @@ fun Fragment.toast(duration: Int = Toast.LENGTH_SHORT, msg: () -> String) {
 
 fun Fragment.activeActivity(lambda: (Activity) -> Unit) {
     activity?.let(lambda) ?: e { "${javaClass.simpleName} not attached to activity" }
+}
+
+fun Context?.greeting(): String {
+    this ?: return ""
+
+    val cal = Calendar.getInstance()
+    return getString(
+        when (cal.get(Calendar.HOUR_OF_DAY)) {
+            in 0..11 -> R.string.text_greeting_morning
+            in 12..15 -> R.string.text_greeting_afternoon
+            in 16..23 -> R.string.text_greeting_evening
+            else -> R.string.text_greeting_night
+        }
+    )
 }
