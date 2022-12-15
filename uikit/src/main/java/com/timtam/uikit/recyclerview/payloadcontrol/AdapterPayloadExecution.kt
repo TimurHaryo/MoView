63548b89600be433e08243fbc611dd74cab54aac
DESKTOP-ED6NHR8\PC <timurharyo00@gmail.com>
2022-12-15 20:00:38 +0700

    add adapter payload executor

---
+++ uikit/src/main/java/com/timtam/uikit/recyclerview/payloadcontrol/AdapterPayloadExecution.kt
@@ -0,0 +1,43 @@
+package com.timtam.uikit.recyclerview.payloadcontrol
+
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @author Timur
+ *
+ * This class is for handle missing payload in recycler view. Recyclerview won't execute/notify
+ * item change (especially with payload) when the target view holder is not bind yet. This class
+ * will save all of the payload along with target position and execute it when [RecyclerView.ViewHolder]
+ * already bind
+ *
+ * [Payload] is any data which devs want to pass
+ * */
+interface AdapterPayloadExecution<Payload> {
+    var recyclerHost: RecyclerView?
+
+    fun enqueueAdapterPayload(
+        targetPosition: Int,
+        payload: Payload,
+        useMainThread: Boolean = true
+    )
+
+    fun executePendingPayload(position: Int)
+
+    /**
+     * Register @Nullable [RecyclerView] to perform inner execution i.e [RecyclerView.Adapter.notifyItemChanged]
+     *
+     * Calls Recommendation: [RecyclerView.Adapter.onAttachedToRecyclerView] AFTER super
+     * */
+    fun registerRecyclerHost(host: RecyclerView?) {
+        recyclerHost = host
+    }
+
+    /**
+     * Register @Nullable [RecyclerView] to perform inner execution i.e [RecyclerView.Adapter.notifyItemChanged]
+     *
+     * Calls Recommendation: [RecyclerView.Adapter.onDetachedFromRecyclerView] BEFORE super
+     * */
+    fun cleanUp() {
+        recyclerHost = null
+    }
+}
---
+++ uikit/src/main/java/com/timtam/uikit/recyclerview/payloadcontrol/AdapterPayloadExecutor.kt
@@ -0,0 +1,77 @@
+package com.timtam.uikit.recyclerview.payloadcontrol
+
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+
+class AdapterPayloadExecutor<Payload> : AdapterPayloadExecution<Payload> {
+    private val pendingPayload: MutableSet<Pair<Int, Payload>> = mutableSetOf()
+
+    override var recyclerHost: RecyclerView? = null
+
+    private val firstVisibleHolder: Int
+        get() {
+            if (recyclerHost?.layoutManager == null) return RecyclerView.NO_POSITION
+            return when (recyclerHost?.layoutManager) {
+                is LinearLayoutManager, is GridLayoutManager -> {
+                    (recyclerHost?.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                }
+                is StaggeredGridLayoutManager -> {
+                    (recyclerHost?.layoutManager as StaggeredGridLayoutManager)
+                        .findFirstVisibleItemPositions(null)
+                        .firstOrNull() ?: RecyclerView.NO_POSITION
+                }
+                else -> RecyclerView.NO_POSITION
+            }
+        }
+
+    private val lastVisibleHolder: Int
+        get() {
+            return if (recyclerHost?.layoutManager != null &&
+                recyclerHost?.layoutManager is LinearLayoutManager
+            ) {
+                (recyclerHost?.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+            } else RecyclerView.NO_POSITION
+        }
+
+    /**
+     * This method will enqueue payload and execute it to targeted position.
+     *
+     * Calls Recommendation: [androidx.fragment.app.Fragment], [androidx.appcompat.app.AppCompatActivity], [RecyclerView.Adapter]
+     * */
+    override fun enqueueAdapterPayload(
+        targetPosition: Int,
+        payload: Payload,
+        useMainThread: Boolean
+    ) {
+        if (targetPosition !in firstVisibleHolder..lastVisibleHolder) {
+            pendingPayload.add(targetPosition to payload)
+            return
+        }
+
+        if (useMainThread) {
+            recyclerHost?.adapter?.notifyItemChanged(targetPosition, payload)
+            return
+        }
+
+        recyclerHost?.post {
+            recyclerHost?.adapter?.notifyItemChanged(targetPosition, payload)
+        }
+    }
+
+    /**
+     * This method has purpose to execute any pending binding in collection based on desired position.
+     * Once executed, the pending payload will be deleted from the list to prevent multiple execution.
+     *
+     * Calls Recommendation: [RecyclerView.Adapter.onViewAttachedToWindow]
+     * */
+    override fun executePendingPayload(position: Int) {
+        pendingPayload.filter { it.first == position }.forEach { (pos, payload) ->
+            recyclerHost?.post {
+                recyclerHost?.adapter?.notifyItemChanged(pos, payload)
+            }
+            pendingPayload.remove(pos to payload)
+        }
+    }
+}
